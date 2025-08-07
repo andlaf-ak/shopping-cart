cfb1f3d91a32640b33408dbb984572adbd1df59b
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-08-07 14:34:20 +0100

    Implement basic shopping cart logic

--- src/shopping_cart/product.py
+++ src/shopping_cart/product.py
@@ -1,9 +1,3 @@
 class Product:
     def __init__(self, name):
         self.name = name
-
-
-class PricedProduct:
-    def __init__(self, product, price):
-        self.product = product
-        self.price = price
--- src/shopping_cart/shopping_cart.py
+++ src/shopping_cart/shopping_cart.py
@@ -1,4 +1,3 @@
-from shopping_cart.product import PricedProduct
 from shopping_cart.receipt import Receipt
 
 
@@ -10,6 +9,13 @@ class ShoppingCart:
         self.products[product.name] = (product, quantity)
 
 def checkout(shopping_cart, pricing_model):
-    items = ['fake item']
-    total = 100
+    items = []
+    total = 0
+    for product, quantity in shopping_cart.products.values():
+        if product.name in pricing_model.policy:
+            price = pricing_model.policy[product.name].amount
+            cost = price * quantity
+            items.append(f"{product.name} at {price} x{quantity} = {cost}")
+            total += cost
     return Receipt(items, total)
+
