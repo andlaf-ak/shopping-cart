0e2274f0120f110b78362d857d4a42bbf0c5ac27
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-08-07 16:11:43 +0100

    Clean up initial boilerplate code

--- src/shopping_cart/currency.py
+++ src/shopping_cart/currency.py
@@ -4,17 +4,3 @@ class Currency:
     def __init__(self, code):
         self.code = code
         self.symbol = SYMBOLS.get(code, code)
-
-    def __eq__(self, other):
-        if isinstance(other, Currency):
-            return self.code == other.code
-        return False
-
-    def __hash__(self):
-        return hash(self.code)
-
-    def __str__(self):
-        return self.code
-
-    def __repr__(self):
-        return f"Currency({self.code})"
